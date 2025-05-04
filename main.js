50611bd7f5d2c58f182c97e0f8c87ff46a95a0b3
Pramodkoyyada <pramodkoyyada666@gmail.com>
2025-05-05 00:57:20 +0530

    Intial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("actionBtn").addEventListener("click", function () {
+    const messageDiv = document.getElementById("message");
+    messageDiv.innerText = "Thanks for choosing this page!";
+    messageDiv.style.color = "#16a34a"; // optional green color on message change
+  });
+  
